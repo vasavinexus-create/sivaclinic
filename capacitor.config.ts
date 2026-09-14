6f96f2d336a22d100d864f11069beeee25847344
gvinothkumar87 <gvinothkumar87@gmail.com>
2026-09-14 12:47:07 +0530

    sdfadfasdfasdfasdf

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.sivacare.clinic",
-  appName: "SivaCare",
+  appName: "MediFlow",
   webDir: "out",
   server: {
     androidScheme: "https",
